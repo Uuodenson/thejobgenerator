fef19902c5b2901485fc43bc51e7b66fb7b54af4
Uuodenson <108183705+Uuodenson@users.noreply.github.com>
2024-06-24 20:32:09 +0200

    d

--- src/components/nav/routes.tsx
+++ src/components/nav/routes.tsx
@@ -1,16 +1,10 @@
 import React from "react";
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import HomePage from "./HomePage";
-import Aufgaben from "./Aufgaben";
-import People from "./People";
-import Out from "./Closeout";
-import NotFoundPage from "./NotFoundPage";
 
 export const AppRoutes: React.FC = () => (
   <Router>
-    <Routes>
-      <HomePage></HomePage>
-    </Routes>
+    <HomePage></HomePage>
   </Router>
 );
 
