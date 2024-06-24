01cc3f8c5ba0ea6f9329629d51a136b12e4fb505
Uuodenson <108183705+Uuodenson@users.noreply.github.com>
2024-06-24 20:02:51 +0200

    test

--- src/components/nav/routes.tsx
+++ src/components/nav/routes.tsx
@@ -1,34 +1,21 @@
 import React from "react";
-import { RouterProvider, createHashRouter } from "react-router-dom";
+import { Route } from "react-router-dom";
 import HomePage from "./HomePage";
 import Aufgaben from "./Aufgaben";
 import People from "./People";
 import Out from "./Closeout";
 import NotFoundPage from "./NotFoundPage";
 
-const router = createHashRouter([
-  {
-    path: "/thejobgenerator",
-    element: <HomePage />,
-    errorElement: <NotFoundPage />,
-  },
-  {
-    path: "aufgaben",
-    element: <Aufgaben />,
-    errorElement: <NotFoundPage />,
-  },
-  {
-    path: "people",
-    element: <People />,
-    errorElement: <NotFoundPage />,
-  },
-  {
-    path: "ausschließen",
-    element: <Out />,
-    errorElement: <NotFoundPage />,
-  },
-]);
-
-export const AppRoutes: React.FC = () => <RouterProvider router={router} />;
+export const AppRoutes: React.FC = () => {
+  return (
+    <>
+      <Route exact path="/" element={<HomePage />} />
+      <Route path="/aufgaben" element={<Aufgaben />} />
+      <Route path="/people" element={<People />} />
+      <Route path="/out" element={<Out />} />
+      <Route path="*" element={<NotFoundPage />} />
+    </>
+  );
+};
 
 export default AppRoutes;
